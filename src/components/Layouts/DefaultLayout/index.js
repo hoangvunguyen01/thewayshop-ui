b2bc51ecd2154069bc1831704e9a81fefd2f9f19
Hoang Vu <hoangvunguyen01@gmail.com>
2023-04-04 15:34:38 +0700

    💄 Add styles

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,14 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from "./DefaultLayout.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx("wrapper")}>
             <Header />
-            <div className="container">
-                <div className="content">{children}</div>
+            <div className={cx("container")}>
+                <div className={cx("content")}>{children}</div>
             </div>
-            <Footer></Footer>
+            <Footer />
         </div>
     );
 }
