aa9406c6ce9545862199112fa15cb0b7fc948f90
Hoang Vu <hoangvunguyen01@gmail.com>
2023-04-08 09:27:05 +0700

    🎨 Update header

--- src/components/Layouts/DefaultLayout/Header/index.js
+++ src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,11 +1,67 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBagShopping, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx("wrapper")}>
-            <div className={cx("inner")}></div>
+            <div className={cx("inner")}>
+                <div className={cx("logo")}>
+                    <img src="/logo.png" alt="TheWay" />
+                </div>
+
+                <div className={cx("navbar-collapse")}>
+                    <ul className={cx("navbar-nav")}>
+                        <li className={cx("nav-item")}>
+                            <a className={cx("nav-link")} href="/">
+                                Home
+                            </a>
+                        </li>
+                        <li className={cx("nav-item")}>
+                            <a className={cx("nav-link")} href="/about">
+                                About Us
+                            </a>
+                        </li>
+                        <li className={cx("nav-item")}>
+                            <a className={cx("nav-link")} href="/">
+                                Product
+                            </a>
+                        </li>
+                        <li className={cx("nav-item")}>
+                            <a className={cx("nav-link")} href="/">
+                                Shop
+                            </a>
+                        </li>
+                        <li className={cx("nav-item")}>
+                            <a className={cx("nav-link")} href="/">
+                                Our Service
+                            </a>
+                        </li>
+                        <li className={cx("nav-item")}>
+                            <a className={cx("nav-link")} href="/">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className={cx("attr-nav")}>
+                    <ul>
+                        <li className="search">
+                            <a href="#">
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </a>
+                        </li>
+                        <li className="cart">
+                            <a href="/cart">
+                                <FontAwesomeIcon icon={faBagShopping} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </header>
     );
 }
